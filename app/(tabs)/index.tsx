ff3c4469babc11497476313118bb9644d4ea53cb
Pushpendra Singh Thakur <pushpendra.singh@myn.global>
2024-12-20 11:53:40 +0530

    Implemented pan and tap gestrues

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,16 @@
-import { StyleSheet, View } from "react-native";
-import ImageViewer from "../components/ImageViewer";
-import Button from "../components/Button";
+import { type ImageSource } from "expo-image";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
-import { type ImageSource } from "expo-image";
+import { StyleSheet, View } from "react-native";
+import Button from "../components/Button";
+import ImageViewer from "../components/ImageViewer";
 
-import IconButton from "../components/IconButton";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import CircleButton from "../components/CircleButton";
-import EmojiPicker from "../components/EmojiPicker";
 import EmojiList from "../components/EmojiList";
+import EmojiPicker from "../components/EmojiPicker";
 import EmojiSticker from "../components/EmojiSticker";
+import IconButton from "../components/IconButton";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -53,7 +54,7 @@ export default function Index() {
   const onSaveImageAsync = () => {};
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           imgSource={PlaceholderImage}
@@ -91,7 +92,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
